64b0d1651622bf32fe685f7267338aaa9283089b
Luke Hooker <luke3106@gmail.com>
2021-03-05 11:16:01 +0000

    feat(setup): add craco.config.js including alias'

---
+++ craco.config.js
@@ -0,0 +1,21 @@
+const path = require(`path`);
+
+// craco.config.js
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@store': path.resolve(__dirname, 'src/store'),
+    }
+  },
+}
