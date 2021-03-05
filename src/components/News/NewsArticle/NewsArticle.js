b2caef09711e3e2df8ff205c69a05a84f3d71276
Luke Hooker <luke3106@gmail.com>
2021-03-05 11:50:21 +0000

    feat(news-feed): style and render NewsArticle contents

--- src/components/News/NewsArticle/NewsArticle.js
+++ src/components/News/NewsArticle/NewsArticle.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-const NewsArticle = () => (
-  <article>
-    News Article will go here
+const NewsArticle = ({ article }) => (
+  <article className="bg-primary rounded-xl p-8 md:p-0 m-4 text-white">
+    <div className="pt-2 md:p-8 text-center md:text-left space-y-4">
+      <section className="font-medium font-bold text-xl">
+        <a href={article.url}>{ article.title }</a>
+      </section>
+      <section>
+        <p className="font-semibold text-white">
+          { article.description }
+        </p>
+      </section>
+      <section className="font-medium italic">
+        { article.author }
+      </section>
+    </div>
   </article>
 );
 
