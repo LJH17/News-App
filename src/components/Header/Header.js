5b2853f31e01984d0d7d3a2fc855f73157d41c5d
Luke Hooker <luke3106@gmail.com>
2021-03-05 11:44:32 +0000

    feat(UI-skeleton): correct created date

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => (
     <div className="flex flex-wrap justify-center">
       <BTLogo className="text-white fill-current" height="75px" />
       <h1 className="px-3 mt-3 md:mt-0 md:ml-3 text-white font-bold md:text-3xl text-center self-center">
-        BT React Code Test - by Luke Hooker - 04/03/2020
+        BT React Code Test - by Luke Hooker - 05/03/2020
       </h1>
     </div>
   </header>
