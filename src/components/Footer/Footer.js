14eed56e9eb709dfc5357e0b67893cc780992311
Luke Hooker <luke3106@gmail.com>
2021-03-05 11:42:38 +0000

    feat(UI-skeleton): create initial Footer component with index.js file

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="bg-primary text-center text-white p-2">
+    Made by Luke Hooker - {new Date().getFullYear()}
+  </footer>
+);
+
+export default Footer;
